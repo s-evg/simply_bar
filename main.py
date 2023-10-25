b4cabd202164241727988d97537924170254a15b
Serg <s-evg@bk.ru>
2023-10-25 23:02:45 +0500

    Update main.py
    Добавлена опция подстановки любого символа

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import sys
 import time
 
 
-def sim_bar(iteration: int, total: list, bar_length: int = 50) -> None:
+def sim_bar(iteration: int, total: list, bar_length: int = 50, symbol: str = "🟦") -> None:
     """Функция для вывода прогресс-бара"""
-    progress = (iteration / total)
-    arrow = '*' * int(round(bar_length * progress))
+    progress = ((iteration + 1) / total)
+    arrow = symbol * int(round(bar_length * progress))
     spaces = ' ' * (bar_length - len(arrow))
     sys.stdout.write(f'\r[{arrow}{spaces}] {int(progress * 100)}%')
     sys.stdout.flush()
@@ -14,7 +14,7 @@ def sim_bar(iteration: int, total: list, bar_length: int = 50) -> None:
 num_list = [_ for _ in range(1, 101)]
 
 for _, num in enumerate(num_list):
-    sim_bar(_ + 1, len(num_list), bar_length=25)
+    sim_bar(_, len(num_list), bar_length=25)
     time.sleep(0.1)
     print(f" [✅️] {num} завершена")
     
